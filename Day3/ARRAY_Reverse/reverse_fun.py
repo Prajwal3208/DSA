4dec53a44f7a97003d669df60553cef2475e007c
Prajwal kadam <prajwalkadam3208@gmail.com>
2025-01-22 09:51:50 +0530

    Create reverse_fun.py

---
+++ Day3/ARRAY_Reverse/reverse_fun.py
@@ -0,0 +1,12 @@
+# Python Program to reverse an array using inbuilt methods
+
+# function to reverse an array
+def reverse_array(arr):
+    arr.reverse()
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverse_array(arr)
+  
+    print(" ".join(map(str, arr)))
